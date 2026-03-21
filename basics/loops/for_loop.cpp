e43eb180b8bbf8da04e60b8d900c34a8e5a333fc
Sudharsshan <sudharsshansy@gmail.com>
2026-03-21 21:50:33 +0530

    added loop statements such as while and for

---
+++ basics/loops/for_loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+
+    // This is a for loop that runs for 50 times printing it's iterative value using cout
+    for (int i = 0; i < 50; i++)
+    {
+        std::cout << "i = " << i << std::endl;
+    }
+    
+    return 0;
+}
---
+++ basics/loops/while_loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    int i = 12; // start with 12 and end with 0
+    while (i > 0)
+    {
+        std::cout << "i = " << i << std::endl;
+        i--;
+    }
+    return 0;
+}
